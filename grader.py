6abe46fd353dfc2b0c8f2cd5027635acc4ef0ca1
Shady Atef <shadyoatef@gmail.com>
2017-03-29 14:36:13 +0200

    Add function to get answers of a specific paper

--- grader.py
+++ grader.py
@@ -1,10 +1,46 @@
 import numpy as np
 import cv2
 import math
+import pathlib
+import csv
 
 # define the answer key which maps the question number
 # to the correct answer
-ANSWER_KEY = {0: 1, 1: 4, 2: 0, 3: 3, 4: 1}
+ANSWER_KEY = {1: 2, 2: 3, 3: 1, 4: 1, 5: 4, 6: 1, 7: 3, 8: 3, 9: 1, 10: 3, 11: 1, 12: 2, 13: 3, 14: 3, 15: 2,
+              16: 1, 17: 4, 18: 2, 19: 3, 20: 2, 21: 4, 22: 3, 23: 4, 24: 2, 25: 4, 26: 3, 27: 4, 28: 4, 29: 2, 30: 3,
+              31: 2, 32: 2, 33: 4, 34: 3, 35: 2, 36: 3, 37: 2, 38: 3, 39: 3, 40: 1, 41: 2, 42: 2, 43: 3, 44: 3, 45: 2}
+
+
+def mark(answers):
+    total_mark = 0
+    for k, v in ANSWER_KEY.items():
+        if answers[k] == v:
+            total_mark += 1
+    return total_mark
+
+
+def sort_contours(cnts, method="left-to-right"):
+    # initialize the reverse flag and sort index
+    reverse = False
+    i = 0
+
+    # handle if we need to sort in reverse
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+
+    # handle if we are sorting against the y-coordinate rather than
+    # the x-coordinate of the bounding box
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+
+    # construct the list of bounding boxes and sort them from top to
+    # bottom
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
+
+    # return the list of sorted contours and bounding boxes
+    return (cnts, boundingBoxes)
 
 
 def display_normal(title, image):
@@ -88,19 +124,121 @@ def crop_answer_section(image):
     return image
 
 
-import os,pathlib
+def have_nearby_contours(contours, needle):
+    (n_x, n_y, n_w, n_h) = cv2.boundingRect(needle)
+
+    for c in contours:
+        (x, y, w, h) = cv2.boundingRect(c)
+        if abs(n_x - x) < 10 and abs(n_y - y) < 10:
+            return True
+    return False
+
+
+def get_answers(image):
+    # display_normal("Just image",image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # median_blurred = cv2.medianBlur(gray, 3)
+    equalized_img = cv2.equalizeHist(gray)
+
+    median = np.median(equalized_img)
+    ret, thresh = cv2.threshold(equalized_img, 0.2 * median, 255, cv2.CHAIN_APPROX_NONE)
+
+    # display_normal("Binary", thresh)
+    # find contours in the thresholded image, then initialize
+    # the list of contours that correspond to questions
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[1]
+
+    questionCnts = []
+
+    # loop over the contours
+    for c in cnts:
+        # compute the bounding box of the contour, then use the
+        # bounding box to derive the aspect ratio
+        (x, y, w, h) = cv2.boundingRect(c)
+        ar = w / float(h)
+        if y < 25 or y > image.shape[0] - 20:
+            continue
+        if x < 10:
+            continue
+        if 200 <= x <= 345:
+            continue
+        if 520 <= x <= 670:
+            continue
+        if len(c) < 5:
+            continue
+        if w < 15 or h < 15:
+            continue
+        if have_nearby_contours(questionCnts, c):
+            continue
+
+        # in order to label the contour as a question, region
+        # should be sufficiently wide, sufficiently tall, and
+        # have an aspect ratio approximately equal to 1
+        questionCnts.append(c)
+
+    if len(questionCnts) != 45 * 4:
+        raise Exception("Didn't found all possible answers")
+
+    answers = {}
+    questionCnts, bounding_boxes = sort_contours(questionCnts, "top-to-bottom")
+    # each question has 4 possible answers, to loop over the
+    # question in batches of 4
+    for (q, i) in enumerate(np.arange(0, len(questionCnts), 12)):
+        # sort the contours for the current question from
+        # left to right, then initialize the index of the
+        # bubbled answer
+        cnts = sort_contours(questionCnts[i:i + 12])[0]
+        for i in np.arange(0, 12, 4):
+            question = q + (i // 4) * 15 + 1
+            single_q_cnts = cnts[i:i + 4]
+            bubbled = None
+            # loop over the sorted contours
+            for (j, c) in enumerate(single_q_cnts):
+                # construct a mask that reveals only the current
+                # "bubble" for the question
+                mask = np.zeros(thresh.shape, dtype="uint8")
+                cv2.drawContours(mask, [c], -1, 255, -1)
+
+                # apply the mask to the thresholded image, then
+                # count the number of non-zero pixels in the
+                # bubble area
+                mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+                total = cv2.countNonZero(mask)
+                # if the current total has a larger number of total
+                # non-zero pixels, then we are examining the currently
+                # bubbled-in answer
+                if bubbled is None or total > bubbled[0]:
+                    bubbled = (total, j + 1)
+
+            answers[question] = bubbled[1]
+
+    return answers
+
+
 failed_images = 0
-tried_files=0
-for f in pathlib.Path('test').iterdir():
-    tried_files+=1
+tried_files = 0
+grades = {}
+for f in pathlib.Path('train').iterdir():
+    tried_files += 1
     try:
         image = cv2.imread(f.as_posix())
         image = optimize_image_rotation(image)
         image = crop_answer_section(image)
-        #display_normal("Cropped version of image",image)
-
+        answers = get_answers(image)
+        grade = mark(answers)
+        grades[f.name] = grade
     except:
+        grades[f.name] = 0
         print("File {0} failed".format(f.as_posix()))
-        failed_images+=1
+        failed_images += 1
 
-print("{0} files failed to crop".format(failed_images))
+with open('submission.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(["FileName", "Mark"])
+
+    for key, value in grades.items():
+        writer.writerow([key, value])
+print("failed to correct {0} files ".format(failed_images))
